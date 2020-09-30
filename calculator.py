043850945bf4e603f6679a4f8d94f7eff4243ef7
Shams66789 <rajansari66789@gmail.com>
2020-10-01 02:02:03 +0530

    Added some more operators

--- calculator.py
+++ calculator.py
@@ -1,24 +1,42 @@
-
+print("Simple Calculator")
+print("1.Add")
+print("2.Substract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Square")
+print("6.Power")
+print("7.Square Root")
+print("8.Quit")
 while True:
     option = input("Enter add or subtract multiply divide quit :  ")
-    if option == 'quit':
+    if option == 8:
         break
-    elif option == 'add':
+    elif option == 1:
         number1 = float(input("Enter first number: "))
         number2 = float(input("Enter second number: "))
         print(number1 + number2)
-    elif option == 'subtract':
+    elif option == 2:
         number1 = float(input("Enter first number: "))
         number2 = float(input("Enter second number: "))
         print(number1 - number2)
-    elif option == 'multiply':
+    elif option == 3:
         number1 = float(input("Enter first number: "))
         number2 = float(input("Enter second number: "))
         print(number1 * number2)
-    elif option == 'divide':
+    elif option == 4:
         number1 = float(input("Enter first number: "))
         number2 = float(input("Enter second number: "))
         print(number1 / number2)
+    elif option == 5:
+        number1 = float(input("Enter a number: "))
+        print(number1**2)
+    elif option == 6:
+        number1 = float(input("Enter the number to find its value: "))
+        number2 = float(input("Enter a value of power: "))
+        print(number1 ** number2)
+    elif option == 7:
+        number1 = float(input("Enter a number: "))
+        print(number1 ** 0.5)
     else : 
         print("Invalid Input")
 print("EXIT")
