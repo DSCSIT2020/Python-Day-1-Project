321fe518d3767b9de68f869fdac070c0acd2b09f
Deep Choudhury <72141495+deep743@users.noreply.github.com>
2020-10-01 02:40:49 +0530

    added percentage feature in the calculater

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 
 while True:
-    option = input("Enter add or subtract multiply divide quit :  ")
+    option = input("Enter add or subtract multiply divide percentage quit :  ")
     if option == 'quit':
         break
     elif option == 'add':
@@ -19,6 +19,11 @@ while True:
         number1 = float(input("Enter first number: "))
         number2 = float(input("Enter second number: "))
         print(number1 / number2)
+    elif option == 'percentage':
+        number = float(input("Enter first number: "))
+        totalnumber = float(input("Enter total number: "))
+        p= ((number/100)*totalnumber)
+        print("The percentage is= ",p,"%")
     else : 
         print("Invalid Input")
 print("EXIT")
