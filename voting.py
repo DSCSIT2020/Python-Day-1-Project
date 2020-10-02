014791073a6241b0895a79f7c437e225f1645cfa
ALOK KUMAR <56088808+ALOKARYAN51@users.noreply.github.com>
2020-10-02 11:32:13 +0530

    voting systen using python

---
+++ voting.py
@@ -0,0 +1,43 @@
+nominee_1=input("Enter the nominee 1 name: ")
+nominee_2=input("Enter the nominee 2 name: ")
+
+nom_1_votes=0
+nom_2_votes=0
+
+votes_id= [1,2,3,4,5,6,7,8,9,10]
+
+num_of_voter=len(votes_id)
+
+while True:
+    if votes_id==[]:
+        print("voting session over")
+        if nom_1_votes>nom_2_votes:
+            percent=(nom_1_votes/num_of_voter)*100
+            print(nominee_1, "has won","with",percent, "% votes")
+            break
+
+        elif nom_2_votes>nom_1_votes:
+            percent=(nom_2_votes/num_of_voter)*100
+            print(nominee_2, "has won","with",percent, "% votes")
+            break
+
+    else:
+        voter=int(input("Enter your voter id no : "))
+        if voter in votes_id:
+            print("You are a voter")
+            votes_id.remove(voter)
+            vote = int(input("Enter your vote 1 or 2: "))
+            if vote==1:
+                nom_1_votes+=1
+                print("Thank you for casting your vote")
+
+            elif vote==2:
+                nom_2_votes+=1
+                print("Thank you for casting your vote")
+
+        else:
+            print("You are not a voter here or you are aleady voted")
+
+
+            
+
